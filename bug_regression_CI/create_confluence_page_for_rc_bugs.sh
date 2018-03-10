41817940c22fb01f8daeb4887eda741ca81906c5
Cara Wang <wlin@redhat.com>
2018-03-10 22:02:50 +0800

    Change the git way to get the files in the shell scripts
    "git clone https" need the permission setting.
    Change it to wget the zip way

--- bug_regression_CI/create_confluence_page_for_rc_bugs.sh
+++ bug_regression_CI/create_confluence_page_for_rc_bugs.sh
@@ -7,27 +7,18 @@ password=$3
 name=$4
 space=%5
 bug=$1
-echo 
-if [[ -e RC_CI ]]; then
-	echo "====Removing the useless dir============"
-	rm -rf RC_CI
-	echo "=============DONE======================="
-fi
-echo "======Git clone the scripts=============== "
-git clone https://github.com/testcara/RC_CI.git
-if [[ -e RC_CI ]];then
-	echo "======================DONE=================="
-else
-	echo "=============FAILED to get the file. Exit With error=========="
-	exit 1
-fi
-dir=$(pwd)
-cd ${dir}/RC_CI
-if [[  -e "${dir}/RC_CI/bug_regression_CI" ]]; then
+tmp_dir="/tmp/$(date +'%s')"
+mkdir -p ${tmp_dir}
+cd ${tmp_dir}
+echo "===============Download the CI files under $(pwd)"
+wget http://github.com/testcara/RC_CI/archive/master.zip
+unzip master.zip
+cd ${tmp_dir}/RC_CI
+if [[  -e "${tmp_dir}/RC_CI/bug_regression_CI" ]]; then
 	echo "=======The target file has been found==============="
-	cd ${dir}/RC_CI/bug_regression_CI
+	cd ${tmp_dir}/RC_CI/bug_regression_CI
 	echo "=======Generating the confluence page content for bugs============"
-	sudo python generate_confluence_page_for_bugs.py "${bug}"
+	sudo python generate_confluence_page_for_bugs.py ${bugzilla_username} ${password} ${bugs}
 	if [[ -e 'content.txt' ]]; then
 		echo "==============Done========================="
 	else
@@ -36,7 +27,7 @@ if [[  -e "${dir}/RC_CI/bug_regression_CI" ]]; then
 	fi
 fi
 echo "==========Adding page to confluence==========="
-result=$(sudo confluence-cli --wikiurl="https://docs.engineering.redhat.com" -u ${user} -p ${password}  addpage -n "Bugs Regression Testing - ${name}" -s ${space}-f 'content.txt')
+result=$(sudo confluence-cli --wikiurl="https://docs.engineering.redhat.com" -u ${confluence_username} -p ${password}  addpage -n "Bugs Regression Testing - ${et_build_name}" -s ${space} -f 'content.txt')
 if [[ ${result} =~ "https" ]]; then
 	echo "=========================Done: confluence page generated===================="
 	echo "====URL:https://docs.engineering.redhat.com/display/$5/Bugs Regression Testing - $4"
