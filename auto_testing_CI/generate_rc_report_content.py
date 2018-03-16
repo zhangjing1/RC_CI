47f54e1cd02af49333656f723b99251030de6f21
Cara Wang <wlin@redhat.com>
2018-03-16 17:26:59 +0800

    Fix RFE: add the testing eviroments in report

--- auto_testing_CI/generate_rc_report_content.py
+++ auto_testing_CI/generate_rc_report_content.py
@@ -11,26 +11,43 @@ class GenerateRCReportContent():
 		self.test_report =self.ci_jenkins.test_report
 		self.test_table_html = ""
 		self.head_row_html = ""
+		self.test_enviroment_html = ""
 		self.test_report_row_html = ""
+		self.perf_env = '''
+		<tr>ET Server: errata-stage-perf.host.stage.eng.bos.redhat.com</tr>
+		<tr>ET DB: errata-stage-perf-db.host.stage.eng.bos.redhat.com</tr>
+		'''
+		self.robotframework_uat_env = 'ET Server: errata-web-01.host.qe.eng.pek2.redhat.com'
+		self.e2e_env = '''
+		<tr>ET Server: et-e2e.usersys.redhat.com</tr>
+		<tr>Pub Server: pub-e2e.usersys.redhat.com</tr>
+		'''
+		self.bug_regression_env = 'ET Server: errata-web-03.host.qe.eng.pek2.redhat.com'
+		self.ts2_uat_env = 'ET server: et-system-test-qe-02.usersys.redhat.com'
+		self.env_options = {'Performance Baseline Testing':self.perf_env, 'TS2.0 UAT Testing':self.ts2_uat_env,
+		                    'RobotFramework UAT Testing':self.robotframework_uat_env, 'E2E Testing':self.e2e_env,
+		                    'Bug Regression Testing':self.bug_regression_env}
 
 	def generate_head_row_html(self):
-		table_column = ['Test Type', 'Test Result', 'Test Result Url', 'Comments']
+		table_column = ['Test Type', 'Test Result', 'Test Result Url', 'Test Enviroment','Comments']
 		head_row = ""
 		for column_name in table_column:
 			head_row += "<th colspan='1'>" + column_name +"</th>"
 		self.head_row_html = "<tr>" + head_row + "</tr>"
 
-	def genreate_test_report_row_html(self):
+	def generate_test_report_row_html(self):
 		test_table_row_content_body = ""
 		for item in self.test_report:
 			test_table_row_content_body += "<td>" + item + "</td>"
+        # add the env column
+		test_table_row_content_body += "<td>" + self.env_options[self.test_report[0]] + "</td>"
 		#left one empty td for "comment"
 		test_table_row_content_body += "<td>" + "" + "</td>"
 		self.test_report_row_html = "<tr>" + test_table_row_content_body + "</tr>"
 
 	def generate_test_report_html(self):
 		self.generate_head_row_html()
-		self.genreate_test_report_row_html()
+		self.generate_test_report_row_html()
 		self.test_table_html = "<table><tbody>" + self.head_row_html + self.test_report_row_html + "</tbody></table>"
 
 	def write_page_file(self):
