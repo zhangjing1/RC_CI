f2564390dcd48bca12c27bc7c506142ea7c5e147
Cara Wang <wlin@redhat.com>
2018-03-20 13:03:57 +0800

    To show more detail to help debug steps

--- auto_testing_CI/generate_rc_report_content_for_all_testings.py
+++ auto_testing_CI/generate_rc_report_content_for_all_testings.py
@@ -5,17 +5,22 @@ class GenerateAllReports():
 	def __init__(self):
 		self.general_reports_name = "general_report_content.txt"
 		self.general_reports_content = ""
+		self.report_files_list = []
 		self.head_column = "<tr><th colspan='1'>Test Type</th><th colspan='1'>Test Result</th><th colspan='1'>Test Result Url</th><th colspan='1'>Test Enviroment</th><th colspan='1'>Comments</th></tr>"
 	
 	def generate_all_reports(self):
-		report_files_list = glob.glob("/tmp/*/RC_CI-master/auto_testing_CI/*_content.txt")
+		self.report_files_list = glob.glob("/tmp/*/RC_CI-master/auto_testing_CI/*_content.txt")
+		print "==========Find the following report files:=========="
+		for file in self.report_files_list:
+			print file
 		#report_files_list = glob.glob("*_content.txt")
-		if len(report_files_list) == 1:
+		if len(self.report_files_list) == 1:
 			print "==== Here just one report, copy it as the general report ===="
-			copyfile(report_files_list[0], str(self.general_reports_name))
-		elif len(report_files_list) > 1:
+			copyfile(self.report_files_list[0], str(self.general_reports_name))
+		    print self.report_files_list[0]
+		elif len(self.report_files_list) > 1:
 			print "==== There are some reports, generate the general report for all testing reports"
-			for file in report_files_list:
+			for file in self.report_files_list:
 				print file
 				report = open(file)
 				report_content = report.read()
@@ -26,6 +31,8 @@ class GenerateAllReports():
 				print self.general_reports_content
 				self.general_reports_content += remove_table_end
 				print self.general_reports_content
+		else:
+			print len(self.report_files_list)
 
 		self.general_reports_content = "<table><tbody>" + self.head_column + self.general_reports_content + "</tbody></table>"
 		print self.general_reports_content
