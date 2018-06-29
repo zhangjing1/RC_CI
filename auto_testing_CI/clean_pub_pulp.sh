f549342697953286702e4b3c4431ba69a4a4cd80
Cara Wang <wlin@redhat.com>
2018-06-29 22:27:43 +0800

    Update the 2 files for initialize pub and pulp CI
    we start to use container slave here. so update the scripts to meet it

--- auto_testing_CI/clean_pub_pulp.sh
+++ auto_testing_CI/clean_pub_pulp.sh
@@ -1,47 +1,28 @@
-#!/bin/bash
-echo "== Begin to clean the pulp env ${server_name} =="
-echo "== first prepare the env =="
-for package in python-devel  python-virtualenv yum-utils
-do
-        if ! [[ $( rpm -qa | grep ${package} ) =~ "${package}" ]];then
-                yum install -y ${package}
-        else
-                echo $( rpm -qa | grep ${package})
-        fi
-done
-# in one-click env, we need to upgrade 'koji' before we can install brewkoji
-if ! [[ $( rpm -qa | grep 'brewkoji') =~ "brewkoji" ]];then
-        yum upgrade koji
-        yum install -y brewkoji
-else
-        echo $( rpm -qa | grep 'brewkoji' )
-fi
-if [[ -e content-delivery-qe ]]; then
-        echo "do nothing"
-       rm -rf content-delivery-qe
-fi
-curl -L -O http://download.devel.redhat.com/rel-eng/RCMTOOLS/rcm-tools-rhel-7-server.repo
 echo "== second get the clean scripts =="
 git clone git://git.app.eng.bos.redhat.com/content-delivery-qe
 cd content-delivery-qe
 echo "== run the cases of clean data =="
 virtualenv --system-site-packages ~/testpython
 . ~/testpython/bin/activate
+pip install setuptools --upgrade
+pip install funcsigs
+sed -i "s/==3.0.3//" test-requirements.txt
 pip install -r test-requirements.txt
 # change the host of the config
-sudo sed -i "s/pub.host.qe.eng.pek2.redhat.com/pub-e2e.usersys.redhat.com/g" /root/content-delivery-qe/unit_tests/configs/QA_01.conf
-sudo sed -i "s/pub-02.host.qe.eng.pek2.redhat.com/pub-e2e.usersys.redhat.com/g" /root/content-delivery-qe/unit_tests/configs/QA_01.conf
-sudo sed -i "s/pub-test.usersys.redhat.com/pub-e2e.usersys.redhat.com/g" /root/content-delivery-qe/unit_tests/configs/QA_01.conf
-sudo sed -i "s/pulp-02.host.qe.eng.pek2.redhat.com/pulp-e2e.usersys.redhat.com/g" /root/content-delivery-qe/unit_tests/configs/QA_01.conf
-sudo sed -i "s/pulp-03.host.qe.eng.pek2.redhat.com/pulp-docker-e2e.usersys.redhat.com/g" /root/content-delivery-qe/unit_tests/configs/QA_01.conf
-sudo sed -i "s/password=redhat/password=errata/g" /root/content-delivery-qe/unit_tests/configs/QA_01.conf
+sed -i "s/pub.host.qe.eng.pek2.redhat.com/pub-e2e.usersys.redhat.com/g" /workdir/content-delivery-qe/unit_tests/configs/QA_01.conf
+sed -i "s/pub-02.host.qe.eng.pek2.redhat.com/pub-e2e.usersys.redhat.com/g" /workdir/content-delivery-qe/unit_tests/configs/QA_01.conf
+sed -i "s/pub-test.usersys.redhat.com/pub-e2e.usersys.redhat.com/g" /workdir/content-delivery-qe/unit_tests/configs/QA_01.conf
+sed -i "s/pulp-02.host.qe.eng.pek2.redhat.com/pulp-e2e.usersys.redhat.com/g" /workdir/content-delivery-qe/unit_tests/configs/QA_01.conf
+sed -i "s/pulp-03.host.qe.eng.pek2.redhat.com/pulp-docker-e2e.usersys.redhat.com/g" /workdir/content-delivery-qe/unit_tests/configs/QA_01.conf
+sed -i "s/password=redhat/password=errata/g" /workdir/content-delivery-qe/unit_tests/configs/QA_01.conf
 # disable some lines which bring some errors
-sudo sed -i 's/"errata"/#"errata"/g'  /root/content-delivery-qe/unit_tests/helpers/constants.py
-sudo sed -i 's/"pulp"/#"pulp"/g'  /root/content-delivery-qe/unit_tests/helpers/constants.py
+sed -i 's/"errata"/#"errata"/g'  /workdir/content-delivery-qe/unit_tests/helpers/constants.py
+sed -i 's/"pulp"/#"pulp"/g'  /workdir/content-delivery-qe/unit_tests/helpers/constants.py
 # disable one useless testing types
-sed -i "/clear_akamai_cdn/d" /root/content-delivery-qe/unit_tests/helpers/test_run_helper.py
+sed -i "/clear_akamai_cdn/d" /workdir/content-delivery-qe/unit_tests/helpers/test_run_helper.py
+echo "setuptools=="
 # clean the pulp and pulp-docker data
-cd /root/content-delivery-qe
+cd /workdir/content-delivery-qe
 py.test -v unit_tests/tests/clean_data.py::CleanData::test_clean_rhsm_pulp_data
 py.test -v unit_tests/tests/clean_data.py::CleanData::test_clean_docker_pulp_data
 echo "== The pulp env is clean =="
---
+++ auto_testing_CI/initial_pub_pulp.sh
@@ -0,0 +1,250 @@
+#!/bin/bash
+# first, get the product version of pub & pulp
+# second, get the installed pub & pulp version on e2e env
+# finally, do the upgrade/downgrade to make sure the two kinds of version are the same
+set -eo pipefail
+
+confluence_wikiurl="https://docs.engineering.redhat.com"
+e2e_version_page="Version of Applications in E2E"
+e2e_version_page_space="~lzhuang"
+
+install_scripts_env() {
+	 pip install --upgrade pip
+	 pip install confluence-py
+	 pip install python-jenkins
+	if [[ $(wget --version | head -1) =~ "GNU Wget" ]]; then
+		echo "=====wget has been installed======";
+	else
+		echo "=====wget has not been installed, Would intall git======"
+		 yum install wget -y
+	fi
+}
+
+get_all_product_versions_content() {
+	echo "confluence-cli --wikiurl=${confluence_wikiurl} -u ${username} -p ${password}  getpagecontent -n "${e2e_version_page}"  -s ${e2e_version_page_space} > pub_pulp_version_content.txt"
+	confluence-cli --wikiurl=${confluence_wikiurl} -u ${username} -p ${password}  getpagecontent -n "${e2e_version_page}"  -s ${e2e_version_page_space} > pub_pulp_version_content.txt
+}
+
+# check the pub version
+get_build_installed_on_server() {
+    echo "checking ---- ${1}"
+	echo $(ssh root@${1} "rpm -qa | grep ${2} | sed 's/.noarch//'")
+
+}
+
+download_ci_files() {
+	echo "=============== Download the CI files under $(pwd) =========="
+	wget http://github.com/testcara/RC_CI/archive/master.zip
+	unzip master.zip
+	cd RC_CI-master/auto_testing_CI
+    echo "---------------done--------------"
+}
+
+initialize_ansible_related_varables(){
+	pub_product_version=""
+	pub_deploy=false
+	pulp_for_rpm_production=""
+	pulp_for_rpm_deploy=false
+	pulp_for_rpm_deploy_downgrade=false
+	pulp_rpm_production=""
+	pulp_rpm_deploy=false
+	pulp_cdn_distributor_production=""
+	pulp_cdn_deploy=false
+	pulp_for_docker_deploy=false
+	pulp_for_docker_production=""
+	pulp_docker_production=""
+	pulp_docker_deploy=false
+	pulp_docker_server_ansible=""
+	pulp_rpm_server_ansible=""
+	pub_ansible=""
+	pulp_for_rpm_ansible=""
+	pulp_rpm_ansible=""
+	pulp_cdn_deploy_ansible=""
+	pulp_for_docker_ansible=""
+	pulp_docker_ansible=""
+}
+
+# check pub related
+check_and_initialize_pub() {
+	pub_installed=$( get_build_installed_on_server ${pub_server} pub-hub )
+	echo "=== pub installed =="
+	echo ${pub_installed}
+	pub_product=$(  python get_pub_pulp_product_version.py ${current_dir}/pub_pulp_version_content.txt pub )
+	echo "== pub production =="
+	echo ${pub_product}
+	if [[ ${pub_installed} =~ ${pub_product} ]]; then
+		echo "== The pub installed is the same as the production, no need to update it =="
+	else
+		pub_deploy=true
+	    echo "== we need to update the pub version =="
+	    pub_product_version=$( echo ${pub_product} | cut -d "-" -f 3- | cut -d '.' -f 1-3 )
+        echo "== will initialize the pub server with pub version ${pub_product_version}"
+	    pub_installed_version=$( echo ${pub_installed} |  cut -d "-" -f 3- | cut -d '.' -f 1-3 )
+	    pub_product_sub_version=$( echo ${pub_product_version} | cut -d '-' -f 2 )
+	    pub_product_ansible_version=$( echo ${pub_product_version} | cut -d '-' -f 1 | cut -d '-' -f 1)
+	    if [[ ${pub_product_sub_version} -gt 1 ]]; then
+	    	pub_ansible="ansible-playbook -u root -i ${WORKSPACE}/inventory/pub ${WORKSPACE}/playbooks/pub/e2e/deploy-pub-e2e.yml -e pub_version=${pub_product_ansible_version} -e pub_release=${pub_product_sub_version}"
+	    else
+	    	pub_ansible="ansible-playbook -u root -i ${WORKSPACE}/inventory/pub ${WORKSPACE}/playbooks/pub/e2e/deploy-pub-e2e.yml -e pub_version=${pub_product_ansible_version}"
+	    fi
+        pub_product_version_integer=$(echo ${pub_product_version} | sed "s/[^0-9]*//g")
+        pub_installed_version_integer=$(echo ${pub_installed_version} | sed "s/[^0-9]*//g" | cut -c 1-${#pub_product_version_integer})
+
+	    if [[ ${pub_installed_version_integer} -gt ${pub_product_version_integer} ]]; then
+	    	echo "== The installed pub is newer to production pub, we need to downgrade it =="
+	    	pub_ansible="${pub_ansible} -e pub_downgrade=true"
+	    fi
+	fi
+	if [[ ${pub_deploy} == "true" ]]; then
+		echo "Ansible: ${pub_ansible}"
+		${pub_ansible}
+		echo "== Now the pub installed is: =="
+		echo $( get_build_installed_on_server ${pub_server} pub-hub )
+	fi
+}
+
+# check pulp-rpm related
+check_and_initialize_pulp_rpm() {
+	pulp_for_rpm_installed=$( get_build_installed_on_server ${pulp_rpm_server}  pulp-server )
+	echo "== pulp installed on pulp-rpm server =="
+	echo ${pulp_for_rpm_installed}
+	pulp_for_rpm_production=$(  python get_pub_pulp_product_version.py ${current_dir}/pub_pulp_version_content.txt pulp_for_rpm )
+	echo "== pulp production =="
+	echo ${pulp_for_rpm_production}
+
+	if [[ ${pulp_for_rpm_installed} =~ ${pulp_for_rpm_production} ]]; then
+		echo "== The pulp server installed is the same as the pulp production, no need to update it =="
+	else
+		pulp_for_rpm_deploy=true
+		pulp_for_rpm_production_name=$( echo ${pulp_for_rpm_production} | sed "s/-server//" )
+		pulp_for_rpm_ansible=" -e pulp_build=${pulp_for_rpm_production_name}"
+		pulp_for_rpm_production_integer=$(echo ${pulp_for_rpm_production} | sed "s/[^0-9]*//g")
+		pulp_for_rpm_installed_integer=$(echo ${pulp_for_rpm_installed} | sed "s/[^0-9]*//g" | cut -c "1-${#pulp_for_rpm_production_integer}")
+		if [[ ${pulp_for_rpm_installed_integer} -gt ${pulp_for_rpm_production_integer} ]];then
+			pulp_for_rpm_ansible=${pulp_for_rpm_ansible}+" -e pulp_downgrade=true"
+		fi
+	fi
+
+	pulp_rpm_installed=$( get_build_installed_on_server ${pulp_rpm_server} pulp-rpm-plugins)
+	echo "== pulp-rpm installed on pulp-rpm server =="
+	echo ${pulp_rpm_installed}
+	pulp_rpm_production=$(  python get_pub_pulp_product_version.py ${current_dir}/pub_pulp_version_content.txt pulp-rpm-plugins )
+	echo "== pulp-rpm production =="
+	echo ${pulp_rpm_production}
+
+	if [[ ${pulp_rpm_installed} =~ ${pulp_rpm_production} ]]; then
+		echo "== The pulp-rpm  installed is the same as the pulp production, no need to update it =="
+	else
+		pulp_rpm_deploy=true
+		pulp_rpm_production_name=$( echo ${pulp_rpm_production} | sed "s/-plugins//" )
+		pulp_rpm_ansible=" -e pulp_rpm_build=${pulp_rpm_production_name}"
+		pulp_rpm_production_integer=$(echo ${pulp_rpm_production} | sed "s/[^0-9]*//g")
+		pulp_rpm_installed_integer=$(echo ${pulp_rpm_installed}   | sed "s/[^0-9]*//g" | cut -c "1-${#pulp_rpm_production_integer}")
+		if [[ ${pulp_rpm_installed_integer} -gt ${pulp_rpm_production_integer} ]];then
+			pulp_rpm_ansible="${pulp_rpm_ansible} -e pulp_downgrade=true"
+		fi
+	fi
+
+	pulp_cdn_distributor_installed=$( get_build_installed_on_server ${pulp_rpm_server} pulp-cdn-distributor-plugins)
+	echo "== pulp-cdn-distributor-plugins installed on pulp-rpm server =="
+	echo ${pulp_cdn_distributor_installed}
+	pulp_cdn_distributor_production=$(  python get_pub_pulp_product_version.py ${current_dir}/pub_pulp_version_content.txt pulp-cdn-distributor-plugins )
+	echo "== pulp-cdn-distributor-plugin production =="
+	echo ${pulp_cdn_distributor_installed}
+	if [[ ${pulp_cdn_distributor_installed} =~ ${pulp_cdn_distributor_production} ]]; then
+		echo "== The pulp-cdn-distributor-plugins installed is the same as the pulp production, no need to update it =="
+	else
+		pulp_cdn_deploy=true
+		pulp_cdn_distributor_build_name=$(echo ${pulp_cdn_distributor_production} | sed "s/-plugins//")
+		pulp_cdn_deploy_ansible=" -e pulp_cdn_distributor_build=${pulp_cdn_distributor_build_name}"
+		pulp_cdn_distributor_production_integer=$(echo ${pulp_cdn_distributor_production} | sed "s/[^0-9]*//g")
+		pulp_cdn_distributor_installed_integer=$(echo ${pulp_cdn_distributor_installed} | sed "s/[^0-9]*//g" | cut -c "1-${#pulp_cdn_distributor_production_integer}")
+
+		if [[ ${pulp_cdn_distributor_installed_integer} -gt ${pulp_cdn_distributor_production_integer} ]];then
+			pulp_cdn_deploy_ansible="${pulp_cdn_deploy_ansible} -e pulp_downgrade=true"
+		fi
+	fi
+	pulp_rpm_server_ansible="ansible-playbook -u root -i ${WORKSPACE}/inventory/pulp ${WORKSPACE}/playbooks/pulp/deploy-pulp-rpm-e2e.yml \
+                         ${pulp_for_rpm_ansible} ${pulp_rpm_ansible} ${pulp_cdn_deploy_ansible}"
+    if [[ ${pulp_for_rpm_deploy} == "true" ]] || [[ ${pulp_rpm_deploy} == "true" ]] || [[ ${pulp_cdn_deploy} == "true" ]];then
+    	echo "== Ansible: ${pulp_rpm_server_ansible} =="
+    	${pulp_rpm_server_ansible}
+    	echo "== Now the pulp-rpm related builds installed are:"
+    	echo $( get_build_installed_on_server ${pulp_rpm_server}  pulp-server )
+    	echo $( get_build_installed_on_server ${pulp_rpm_server} pulp-rpm-plugins)
+    	echo $( get_build_installed_on_server ${pulp_rpm_server} pulp-cdn-distributor-plugins)
+    fi
+}
+# check pulp-docker related
+check_and_initialize_pulp_docker() {
+	pulp_for_docker_installed=$( get_build_installed_on_server ${pulp_docker_server} ${server_password} pulp-server )
+	echo "== pulp installed on pulp-docker server =="
+	echo ${pulp_for_docker_installed}
+	pulp_for_docker_production=$(  python get_pub_pulp_product_version.py ${current_dir}/pub_pulp_version_content.txt pulp_for_docker )
+	echo "== pulp production =="
+	echo ${pulp_for_docker_production}
+	if [[ ${pulp_for_docker_installed} =~ ${pulp_for_docker_production} ]]; then
+		echo "== The pulp server installed is the same as the pulp production, no need to update it =="
+	else
+		pulp_for_docker_deploy=true
+		pulp_for_docker_ansible=" -e pulp_build=${pulp_for_docker_production}"
+		pulp_for_docker_production_integer=$(echo ${pulp_for_docker_production} | sed "s/[^0-9]*//g")
+		pulp_for_docker_installed_integer=$(echo ${pulp_for_docker_installed} | sed "s/[^0-9]*//g" | cut -c "1-${#pulp_for_docker_production_integer}")
+		if [[ ${pulp_for_docker_installed_integer} -gt ${pulp_for_docker_production_integer} ]];then
+			pulp_for_docker_ansible=${pulp_for_docker_ansible}+" -e pulp_downgrade=true"
+		fi
+	fi
+
+	pulp_docker_installed=$( get_build_installed_on_server ${pulp_docker_server} ${server_password} pulp-docker-plugins)
+	echo "== pulp-docker installed on pulp-docker server =="
+	echo ${pulp_docker_installed}
+	pulp_docker_production=$(  python get_pub_pulp_product_version.py ${current_dir}/pub_pulp_version_content.txt pulp-docker-plugins )
+	echo "== pulp-docker production =="
+	echo ${pulp_docker_production}
+
+	if [[ ${pulp_docker_installed} =~ ${pulp_docker_production} ]]; then
+		echo "== The pulp-docker  installed is the same as the pulp production, no need to update it =="
+	else
+		pulp_docker_deploy=true
+		pulp_docker_ansible=" -e pulp_docker_build=${pulp_docker_production}"
+		pulp_docker_production_integer=$(echo ${pulp_docker_production} | sed "s/[^0-9]*//g")
+		pulp_docker_installed_integer=$(echo ${pulp_docker_installed}   | sed "s/[^0-9]*//g" | cut -c "1-${#pulp_docker_production_integer}")
+		if [[ ${pulp_docker_installed_integer} -gt ${pulp_docker_production_integer} ]];then
+			pulp_docker_ansible=${pulp_docker_ansible}+" -e pulp_downgrade=true"
+		fi
+	fi
+
+	pulp_docker_server_ansible="ansible-playbook -u root -i ${WORKSPACE}/inventory/pulp ${WORKSPACE}/playbooks/pulp/deploy-pulp-docker-e2e.yml \
+    ${pulp_for_docker_ansible} ${pulp_docker_ansible}"
+    if [[ ${pulp_docker_deploy} == "true" ]] || [[ ${pulp_for_docker_deploy} == "true" ]];then
+    	echo "== Ansible: ${pulp_docker_server_ansible} =="
+    	${pulp_docker_server_ansible}
+    	
+    	echo "== Now the pulp-docker related builds installed are: =="
+    	echo $( get_build_installed_on_server ${pulp_docker_server} pulp-server )
+        echo $( get_build_installed_on_server ${pulp_docker_server} pulp-docker-plugins)
+    fi
+}
+
+clean_running_and_free_pub_tasks() {
+    free_running_tasks=$(ssh root@${pub_server} "pub list-tasks  --free --running --user errata --password errata | tr '\n' ' '")
+    if [[ -z ${free_running_tasks} ]]; then
+        echo "== There is no running or free tasks on the pub server"
+    else
+        echo "== There are some running and free tasks on the pub server, cleaning it"
+        ssh root@${pub_server} "pub cancel-tasks --user errata --password errata ${free_running_tasks}"
+        echo "== Done for pub tasks clean"
+    fi
+}
+
+
+
+install_scripts_env
+download_ci_files
+initialize_ansible_related_varables
+current_dir=$( echo `pwd` )
+clean_running_and_free_pub_tasks
+get_all_product_versions_content
+check_and_initialize_pub
+check_and_initialize_pulp_rpm
+check_and_initialize_pulp_docker
