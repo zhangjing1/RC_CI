2ae854a3b745df32b19effc2a0615fae7a115f22
Cara Wang <wlin@redhat.com>
2018-06-30 21:24:30 +0800

    Add parser_performance_report sh for container

---
+++ auto_testing_CI/parser_performance_report.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -eo pipefail
+IFS=$'\n\t'
+
+install_scripts_env(){
+	sudo pip install --upgrade pip
+	sudo pip install confluence-py
+	sudo pip install python-jenkins
+	sudo pip install scp
+	yum install wget -y
+	yum install git -y
+}
+
+initial_et_build_version(){
+	if [[ ${et_build_name_or_id} =~ "-" ]]; then
+		echo "=== ET build name has been provided: ${et_build_name} =="
+		et_build_version=$( echo ${et_build_name_or_id} | cut -d '-' -f 2| cut -d '.' -f 2 )
+	else
+		echo "=== ET build id is provided =="
+		et_build_version=${et_build_name_or_id}
+	fi
+}
+
+
+run_target_scripts(){
+	sudo python /workdir/RC_CI-master/auto_testing_CI/talk_to_rc_jenkins_to_parser_perf_report.py ${username} ${password} ${et_build_version} ${tolerance} ${max_accepted_time} ${perf_jmeter_slave_server}
+}
+
+
+install_scripts_env
+initial_et_build_version
+download_ci_scripts
+run_target_scripts
