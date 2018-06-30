410080ddac1ddf9ce228751d424da56a7843c92e
Cara Wang <wlin@redhat.com>
2018-06-30 21:51:02 +0800

    remove "sudo" commands in the script

--- auto_testing_CI/parser_performance_report.sh
+++ auto_testing_CI/parser_performance_report.sh
@@ -3,10 +3,10 @@ set -eo pipefail
 IFS=$'\n\t'
 
 install_scripts_env(){
-	sudo pip install --upgrade pip
-	sudo pip install confluence-py
-	sudo pip install python-jenkins
-	sudo pip install scp
+	pip install --upgrade pip
+	pip install confluence-py
+	pip install python-jenkins
+	pip install scp
 	yum install wget -y
 	yum install git -y
 }
@@ -23,7 +23,7 @@ initial_et_build_version(){
 
 
 run_target_scripts(){
-	sudo python /workdir/RC_CI-master/auto_testing_CI/talk_to_rc_jenkins_to_parser_perf_report.py ${username} ${password} ${et_build_version} ${tolerance} ${max_accepted_time} ${perf_jmeter_slave_server}
+	 python /workdir/RC_CI-master/auto_testing_CI/talk_to_rc_jenkins_to_parser_perf_report.py ${username} ${password} ${et_build_version} ${tolerance} ${max_accepted_time} ${perf_jmeter_slave_server}
 }
 
 
