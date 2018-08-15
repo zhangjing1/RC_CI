2a7c1b40ec47abb0bfda1ffdbe9930e02af53291
Cara Wang <wlin@redhat.com>
2018-08-15 10:12:15 +0800

    RFE: add flags to spike and otherQA bugs
    For spkie and otherQA bug, flag the 'qe_auto_coverage' as '-'
    Then Let CI_3 deal the latest data.

--- auto_testing_CI/generate_confluence_page_for_bugs.py
+++ auto_testing_CI/generate_confluence_page_for_bugs.py
@@ -32,6 +32,26 @@ def get_bug_and_format_bug(bug_id):
 	bug_result = ""
 	bzapi = bugzilla.Bugzilla("bugzilla.redhat.com")
 	bug = bzapi.getbug(int(bug_id))
+	# for spike tasks, Let the script add 'qe_auto_coverage' flag automatically if needed
+	if bug.summary.find('[Spike]') == 0 and type(bug.get_flags('qe_auto_coverage')) == type(bug.get_flags('abcdefg')):
+		# For spike bug, if the 'qe_auto_coverage' flag has not been set, set the flag automatically
+		print "Add the flag 'qe_auto_coverage' as '-' to the spike bug {}".format(bug.id)
+		auto_flag = {}
+		auto_flag['qe_auto_coverage']='-'
+		bug.updateflags(auto_flag)
+		# refetch the bug
+		bug = bzapi.getbug(bug.id)
+
+	# for otherQA bug, Let the script add 'qe_auto_coverage' flag as '-' automatically if needed
+	if 'OtherQA' in bug.keywords and type(bug.get_flags('qe_auto_coverage')) == type(bug.get_flags('abcdefg')):
+		# For OtherQA bug, if the 'qe_auto_coverage' flag has not been set, set the flag automatically
+		print "Add the flag 'qe_auto_coverage' as '-' to the otherQA bug {}".format(bug.id)
+		auto_flag = {}
+		auto_flag['qe_auto_coverage'] = '-'
+		bug.updateflags(auto_flag)
+		# refetch the bug
+		bug = bzapi.getbug(bug.id)
+
 	qe_flag = False
 	for flag in bug.flags:
 		if flag['name'] == "qe_auto_coverage":
@@ -45,16 +65,9 @@ def get_bug_and_format_bug(bug_id):
 	regex = re.compile('[^a-zA-Z0-9 _\?\[\]{}()]')
 	summary = regex.sub('', bug.summary)
 
-	if bug_flag == "-" or bug_flag == "+":
+	finished_testing_bugs_status = ['RELEASE_PENDING','VERIFIED','CLOSED']
+	if bug.status in finished_testing_bugs_status and bug_flag in ['+', '-']:
 		bug_result = "PASSED"
-       # deal with some particular situations
-        if bug.status == "CLOSED" and bug_flag == "":
-            bug_result = "PASSED"
-            bug_flag = "NULL"
-        if 'OtherQA' in bug.keywords and bug.status == "VERIFIED" and bug_flag == "":
-            bug_result = "PASSED"
-            bug_flag = "NULL"
-
 
 	formatted_bug = [ bug.id, summary, bug.component, bug.status, bug.severity, bug.priority, bug_flag, bug.qa_contact, bug_result ]
 	return formatted_bug
@@ -65,17 +78,19 @@ def get_bugs_and_format_bugs(bugs_list):
 		formatted_bugs.append(get_bug_and_format_bug(bug))
 	return formatted_bugs
 
+# get all passed bug for the second table
 def get_formatted_automated_bugs_list(bugs_list):
 	formatted_automated_bugs = []
 	for bug in bugs_list:
-		if bug[6] == "-" or bug[6] == "+" or bug[6] == "NULL":
+		if bug[8] == "PASSED":
 			formatted_automated_bugs.append(bug)
 	return formatted_automated_bugs
 
+# get all manual testing bugs for the first table
 def get_formatted_manual_bugs_list(bugs_list):
 	formatted_manual_bugs = []
 	for bug in bugs_list:
-		if bug[6] != "-" and bug[6] != "+" and bug[6] != "NULL":
+		if bug[8] != "PASSED":
 			formatted_manual_bugs.append(bug)
 	return formatted_manual_bugs
 
@@ -123,7 +138,6 @@ def generate_confluence_page_for_bugs(user, password, bugs):
 	info_for_manual_testing_html = '<p>' + "'' and '?' of 'qe_auto_coverage' of the following table means QE have not finished the automation tasks of the bugs." + "<strong>" + " Manual testing is needed! " + "</strong></p>"
 	info_for_automated_testing_html = "<p>" + "'-' of 'qe_auto_coverage' of the following table means QE have confirmed that "+ "<strong>" + "no more manual testing is needed." + "</strong>" + " For dev's autoated testing has been covered or it is minor UI change or unimportant negative case can be ingored! "+ '<strong>' + "Mark as 'PASSED' directly!" +'</strong></p>'
 	info_for_automated_testing_html += "<p>" + "'+' means QE have completed the automation tasks."  + "<strong>" + "TS2.0 has been covered it. Mark as PASSED directly!" + "</strong></p>"
-        info_for_automated_testing_html += "<p>" + "'NULL' means the bug/task has been closed or verified by ET dev." + "<strong>" + "It does not need extral testings or confirmation from QE. Mark as PASSED directly!" + "</strong></p>"
 	page_notice = "'qe_auto_coverage' on the page shows QE automation status for bugs."
 	page_notice_html = "<p>" + page_notice + "</p>"
 	html = page_notice_html + info_for_manual_testing_html + formatted_manual_bugs_html + info_for_automated_testing_html + formatted_automated_bugs_html
