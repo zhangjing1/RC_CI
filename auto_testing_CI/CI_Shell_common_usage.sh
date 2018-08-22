1942f7f31df13578d153482d2b96dd0e8ce1edcd
Cara Wang <wlin@redhat.com>
2018-08-22 20:36:13 +0800

    Fix bug: do not 'echo' too much in the comparison function
    It is really a bad usage. I use 'echo' to share the result.
    If we echo too much, it will affect the results.
    I would like to research other method to return the results

--- auto_testing_CI/CI_Shell_common_usage.sh
+++ auto_testing_CI/CI_Shell_common_usage.sh
@@ -58,18 +58,19 @@ get_ansible_commands_with_build_id(){
 }
 
 compare_version_or_id(){
-    echo "=== Format the compared versions again ==="
+    #echo "=== Format the compared versions again ==="
     first_version=$(printf '%-6d' ${1} | sed 's/ /0/g')
     second_version=$(printf '%-6d' ${2} | sed 's/ /0/g')
-    echo "The first version is: ${first_version}"
-    echo "The second version is: ${second_version}"
+    #echo "The first version is: ${first_version}"
+    #echo "The second version is: ${second_version}"
     comparison_result="downgrade"
     if [[ "${first_version}" == "${second_version}" ]]; then
         comparison_result="same"
     elif [[ "${first_version}" -lt "${second_version}" ]]; then
         comparison_result="upgrade"
     fi
-    echo "=== The comparison result is: ${comparison_result} =="
+    #echo "=== The comparison result is: ${comparison_result} =="
+    echo ${comparison_result}
 }
 
 perf_restore_db() {
